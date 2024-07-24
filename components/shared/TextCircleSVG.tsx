97f5c7f8b1ef5badd97b79a2fe721756c8bf323f
BuiQuangHieu93 <zzzqhieu@gmail.com>
2024-07-24 13:58:00 +0700

    update

--- components/shared/TextCircleSVG.tsx
+++ components/shared/TextCircleSVG.tsx
@@ -5,33 +5,34 @@ const TextCircleSVG: React.FC = () => {
     <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
       <style>
         {`
-          .rotate {
-            animation: rotate 20s linear infinite;
+        .rotate {
+          animation: rotate 20s linear infinite;
+          transform-origin: center; /* Apply transform-origin using CSS */
+        }
+  
+        @keyframes rotate {
+          0% {
+            transform: rotate(0deg);
           }
-
-          @keyframes rotate {
-            0% {
-              transform: rotate(0deg);
-            }
-            25% {
-              transform: rotate(90deg);
-            }
-            50% {
-              transform: rotate(180deg);
-            }
-            75% {
-              transform: rotate(270deg);
-            }
-            100% {
-              transform: rotate(360deg);
-            }
+          25% {
+            transform: rotate(90deg);
           }
-
-          text {
-            fill: white;
-            text-transform: uppercase;
+          50% {
+            transform: rotate(180deg);
           }
-        `}
+          75% {
+            transform: rotate(270deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+  
+        text {
+          fill: white;
+          text-transform: uppercase;
+        }
+      `}
       </style>
       <path
         id="textPath"
@@ -40,7 +41,7 @@ const TextCircleSVG: React.FC = () => {
         fill="none"
         strokeWidth="0"
       ></path>
-      <g fontSize="13.1px" className="rotate" transformOrigin="center">
+      <g fontSize="13.1px" className="rotate">
         <text textAnchor="start">
           <textPath className="coloring" xlinkHref="#textPath" startOffset="0%">
             Furniture &amp; Home Craft . Furniture &amp; Home Craft . Furniture
