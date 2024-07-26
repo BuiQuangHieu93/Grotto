c71aee5bf9fe3b0cf74f3212fb3dbd3a3bcff1bb
BuiQuangHieu93 <zzzqhieu@gmail.com>
2024-07-26 09:22:15 +0700

    update middeware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,17 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/events/:id",
-    "/api/webhooks/clerk",
-    "/api/webhooks/stripe",
-    "/api/uploadthing",
-  ],
-  ignoredRoutes: [
-    "/api/webhooks/clerk",
-    "/api/webhooks/stripe",
-    "/api/uploadthing",
-  ],
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/api/webhooks/clerk",
+  "/api/uploadthing",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
 
 export const config = {
