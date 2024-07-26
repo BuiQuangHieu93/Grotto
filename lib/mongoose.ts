7f7e9fa19c34375eb05bdbaf5cc14ff04050dd60
BuiQuangHieu93 <zzzqhieu@gmail.com>
2024-07-26 08:13:28 +0700

    build clerk webhook

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 let isConnected = false;
 
-export const connectToDB = async () => {
+export const connectToDatabase = async () => {
   mongoose.set("strictQuery", true);
   if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
   if (isConnected) return console.log("Already connected to MongoDB");
